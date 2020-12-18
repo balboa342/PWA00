07de4431fab2b78eca8016549b931fbfb57acf9e
balboa342 <42502796+balboa342@users.noreply.github.com>
2020-12-18 07:37:39 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
+  navigator.serviceWorker.register('sw.js');
 }
 
 document.querySelector('#show').addEventListener('click', () => {
