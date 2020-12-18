f42a3cb3cf28131bab01ee195c2d1e6e7e9bc314
balboa342 <42502796+balboa342@users.noreply.github.com>
2020-12-18 07:34:47 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('service-worker.js');
+}
+
+document.querySelector('#show').addEventListener('click', () => {
+  const iconUrl = document.querySelector('select').selectedOptions[0].value;
+  let imgElement = document.createElement('img');
+  imgElement.src = iconUrl;
+  document.querySelector('#container').appendChild(imgElement);
+});
+
+/*
 window.onload = () => {
 	'use strict';
 	
@@ -5,3 +17,4 @@ window.onload = () => {
 		navigator.serviceWorker.register('./sw.js');
 	}
 }
+*/
