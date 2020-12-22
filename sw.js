6a10682f1233fa16593ade38f5d4b9ecdcb9710e
balboa342 <42502796+balboa342@users.noreply.github.com>
2020-12-22 07:32:34 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,11 @@ const PRECACHE_URLS = [
   './', // Alias for index.html
   'css/style.css',
   'js/main.js',
+  'images/hello-icon-128.png',
+  'images/hello-icon-144.png',
+  'images/hello-icon-152.png',
   'images/hello-icon-192.png',
+  'images/hello-icon-256.png',
   'images/hello-icon-512.png',
   'favicon.ico',
   'sw.js'
